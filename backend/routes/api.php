98d52c1339897f5f597c04a5e166ff9accd33592
Jorge Soto <Jorgemsm19@gmail.com>
2018-12-21 22:15:02 -0400

    Name common routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -11,23 +11,26 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'auth'], function () {
-    Route::post('login', 'AuthController@login');
-    Route::post('signup', 'AuthController@signup');
-    Route::get('signup/activate/{token}', 'AuthController@signupActivate');
+    Route::post('login', 'AuthController@login')->name('login');
+    Route::post('signup', 'AuthController@signup')->name('signup');
+    Route::get('signup/activate/{token}', 'AuthController@signupActivate')
+            ->name('signup_activate');
     Route::get('/unauthorized', 'AuthController@unauthorized')
         ->name('unauthorized');
 
     Route::group(['middleware' => ['auth:api']], function () {
-        Route::get('logout', 'AuthController@logout');
-        Route::get('user', 'AuthController@user');
+        Route::get('logout', 'AuthController@logout')->name('logout');
+        Route::get('user', 'AuthController@user')->name('get_auth_user');
     });
 });
 
 Route::group(['middleware' => ['auth:api', 'role:admin']], function () {
     // Attach new role to User
-    Route::put('/users/{id}/{role}', 'RolesController@addRole');
+    Route::put('/users/{id}/{role}', 'RolesController@addRole')
+        ->name('add_role');
     // Delete new role to User
-    Route::delete('/users/{id}/{role}', 'RolesController@removeRole');
+    Route::delete('/users/{id}/{role}', 'RolesController@removeRole')
+        ->name('remove_role');
 
     Route::apiResource('module', 'ModuleController');
 
