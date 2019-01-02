329167b7dcd4f28fea176a55b047a6c2550cebf1
Jorge Soto <Jorgemsm19@gmail.com>
2019-01-02 15:23:54 -0400

    Frontend: index page now redirects to login

--- frontend/pages/index/index.js
+++ frontend/pages/index/index.js
@@ -1,60 +1,14 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import NProgress from 'nprogress';
-import { Button } from 'antd';
-import { GetData } from '../../utils/fetch';
-import { withNamespaces } from '../../i18n';
+import Router from 'next/router';
 
 export class IndexPage extends Component {
-  state = {
-    greeting: '',
-    loading: false,
-  }
-
-  static async getInitialProps() {
-    return {
-      namespacesRequired: ['common']
-    }
-  }
-
-  getData = () => {
-    this.setState({
-      loading: true
-    });
-    NProgress.start();
-    GetData()
-      .then(data => data.json())
-      .then(json => {
-        this.setState({
-          greeting: json.greeting,
-          loading: false,
-        })
-        NProgress.done();
-      })
-      .catch(error => {
-        this.setState({
-          loading: false,
-        })
-        NProgress.done();
-      })
+  componentDidMount() {
+    Router.push('/login');
   }
 
   render() {
-    const { greeting, loading } = this.state;
-    const { t } = this.props;
-
-    return (
-      <div className="indexPage">
-        <h1>{greeting || 'Hello from Next'}</h1>
-        <Button type="primary" loading={loading}
-          onClick={this.getData}>{t('getData')}</Button>
-      </div>
-    )
+    return <></>;
   }
 }
 
-IndexPage.propTypes = {
-  t: PropTypes.func.isRequired,
-}
-
-export default withNamespaces('common')(IndexPage);
+export default IndexPage;
