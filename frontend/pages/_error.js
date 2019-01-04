8d53e363d0b7b1e2cdb70b58567eb3e0c309a5c7
Jorge Soto <Jorgemsm19@gmail.com>
2019-01-04 14:13:42 -0400

    Frontend: - Custom error page. - Add script on package.json to create _redirects file for netlify.

---
+++ frontend/pages/_error.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Alert } from 'antd';
+
+class ErrorPage extends React.Component {
+  static getInitialProps({ res, err }) {
+    // eslint-disable-next-line no-nested-ternary
+    const statusCode = res ? res.statusCode : err ? err.statusCode : null;
+    return { statusCode }
+  }
+
+  render() {
+    const { statusCode } = this.props;
+
+    return (
+      <Alert
+        style={{ minWidth: 'fit-content', width: '100%', maxWidth: '900px', margin: '0 auto' }}
+        showIcon
+        type="error"
+        message={`Error ${statusCode || ''}`}
+        description="🚧 Page under construction... Maybe 🚧"
+      />
+    )
+  }
+}
+
+ErrorPage.propTypes = {
+  statusCode: PropTypes.number.isRequired,
+}
+
+
+export default ErrorPage;
