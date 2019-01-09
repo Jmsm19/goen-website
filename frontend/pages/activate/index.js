9eddce4de073c7c5a54d605b0cba5b76eec9f494
Jorge Soto <Jorgemsm19@gmail.com>
2019-01-08 20:38:58 -0400

    Frontend: fix token param on static export site

--- frontend/pages/activate/index.js
+++ frontend/pages/activate/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Router, { withRouter } from 'next/router';
 import NProgress from 'nprogress';
@@ -11,50 +11,59 @@ export class AccountActivationPage extends Component {
   state = {
     isActivating: true,
     activationSuccess: false,
-  }
+  };
 
   static async getInitialProps() {
     return {
       namespacesRequired: ['common'],
-    }
+    };
   }
 
   async componentDidMount() {
-    const { router: { query: { tk } }, t } = this.props;
+    const { router, t } = this.props;
+    const { query, asPath } = router;
+    const token = query.tk || asPath.split('=')[1];
+
     try {
       NProgress.start();
-      const response = await GetData(`/auth/signup/activate/${tk}`);
+      const response = await GetData(`/auth/signup/activate/${token}`);
       const { data, message } = await response.json();
 
       if (response.status === 200) {
-        this.setState(() => ({
-          isActivating: false,
-          activationSuccess: true,
-        }), () => {
-          notification.success({
-            message: t('ActivationSuccess'),
-            description: `All good, ${data.name}`,
-            onClose: this.goToLogin,
-          })
-        })
+        this.setState(
+          () => ({
+            isActivating: false,
+            activationSuccess: true,
+          }),
+          () => {
+            notification.success({
+              message: t('ActivationSuccess'),
+              description: `All good, ${data.name}`,
+              onClose: this.goToLogin,
+            });
+          },
+        );
       } else {
         throw Error(message);
       }
     } catch ({ message }) {
-      this.setState(() => ({
-        isActivating: false,
-      }), () => {
-        notification.error({
-          message,
-        })
-      })
+      this.setState(
+        () => ({
+          isActivating: false,
+        }),
+        () => {
+          notification.error({
+            message,
+          });
+        },
+      );
     }
     NProgress.done();
   }
 
   goToLogin = () => {
     Router.push('/login');
-  }
+  };
 
   render() {
     const { activationSuccess, isActivating } = this.state;
@@ -62,31 +71,30 @@ export class AccountActivationPage extends Component {
 
     return (
       <StyledPage>
-        {
-          // eslint-disable-next-line no-nested-ternary
-          isActivating ? (
-            <>
-              <h1>
-                <Icon type="loading" spin
-                  style={{ color: 'var(--primary-color)', paddingRight: '30px' }} />
-                {t('VerifyingAccount')}
-              </h1>
-            </>
-          ) : (
-            activationSuccess ? (
-              <>
-                <h1>{t('AccountActivated')}</h1>
-                <p>{t('Wait a moment you will be redirected to the Login page')}</p>
-              </>
-            ) : (
-              <>
-                <h1>{t('ActivationActivationFailed')}</h1>
-              </>
-            )
-          )
-        }
+        {// eslint-disable-next-line no-nested-ternary
+        isActivating ? (
+          <>
+            <h1>
+              <Icon
+                type='loading'
+                spin
+                style={{ color: 'var(--primary-color)', paddingRight: '30px' }}
+              />
+              {t('VerifyingAccount')}
+            </h1>
+          </>
+        ) : activationSuccess ? (
+          <>
+            <h1>{t('AccountActivated')}</h1>
+            <p>{t('Wait a moment you will be redirected to the Login page')}</p>
+          </>
+        ) : (
+          <>
+            <h1>{t('ActivationActivationFailed')}</h1>
+          </>
+        )}
       </StyledPage>
-    )
+    );
   }
 }
 
@@ -95,8 +103,8 @@ AccountActivationPage.propTypes = {
   router: PropTypes.shape({
     query: PropTypes.shape({
       tk: PropTypes.string,
-    })
-  }).isRequired
-}
+    }),
+  }).isRequired,
+};
 
 export default withNamespaces('commmon')(withRouter(AccountActivationPage));
