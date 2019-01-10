7cc25b608c0fb3c70f15e38e5948e0a5c0738823
Jorge Soto <Jorgemsm19@gmail.com>
2019-01-10 13:09:12 -0400

    Frontend: change styling media util to use px instead of em

--- frontend/utils/styling.js
+++ frontend/utils/styling.js
@@ -1,14 +1,15 @@
 import { css } from 'styled-components';
 
 const sizes = {
-  // desktop: 992,
+  desktop: 992,
   tablet: 768,
   phone: 576,
 };
 
 const media = Object.keys(sizes).reduce((acc, label) => {
   acc[label] = (...args) => css`
-    @media (max-width: ${sizes[label] / 16}em) {
+  ${'' /* @media (max-width: ${sizes[label] / 16}em) { */}
+    @media (max-width: ${sizes[label]}px) {
       ${css(...args)}
     }
   `;
