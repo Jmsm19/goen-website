67b3fc34202945a49967f73610d57e85174663e0
Jorge Soto <Jorgemsm19@gmail.com>
2019-01-16 17:03:56 -0400

    Backend: add instructors and assistants to module seeder

--- backend/database/seeds/ModuleTableSeeder.php
+++ backend/database/seeds/ModuleTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Role;
 use App\Price;
 use App\Module;
 use App\Period;
@@ -22,6 +23,8 @@ class ModuleTableSeeder extends Seeder
         $amount_of_schedules = count($available_schedueles);
         $available_prices = Price::all();
         $amount_of_prices = count($available_prices);
+        $instructor = Role::with('users')->where('name', 'instructor')->first();
+        $assistant = Role::with('users')->where('name', 'assistant')->first();
 
         for ($i=0; $i < 3 ; $i++) {
             $random_schedule = $available_schedueles[round(rand(0, $amount_of_schedules - 1))];
@@ -33,6 +36,8 @@ class ModuleTableSeeder extends Seeder
                 'period_id' => $current_period->id,
                 'price_id' => $random_price->id,
                 'schedule_id' => $random_schedule->id,
+                'instructor_id' => $instructor->id,
+                'assistant_id' => $assistant->id,
             ]);
         }
     }
