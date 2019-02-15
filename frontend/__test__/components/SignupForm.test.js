b690e611f340a6a5e1e060465b1ab4b63bcdbfa2
Jorge Soto <Jorgemsm19@gmail.com>
2019-02-15 14:25:30 -0400

    Frontend: fix SignUpPage imports

--- frontend/__test__/components/SignupForm.test.js
+++ frontend/__test__/components/SignupForm.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-testing-library';
-import SignupForm from '../../components/Forms/SignupForm';
+import SignupForm from '../../components/Forms/SignUpForm';
 import tFunction from '../utils/tFunction';
 
 describe('Test Register Form component', () => {
