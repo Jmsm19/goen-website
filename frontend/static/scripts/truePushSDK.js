aa7d2885be115b9966c0fb24f74993a95a087217
Jorge Soto <Jorgemsm19@gmail.com>
2019-02-26 11:28:45 -0400

    Frontend: remove TruePushSDK

--- frontend/static/scripts/truePushSDK.js
+++
@@ -1,6 +0,0 @@
-const truepushSDK = document.createElement('script');
-truepushSDK.setAttribute(
-  'src',
-  `https://app.truepush.com/sdk/app.js?key=5c3762e31818f4417b62e6bb&reload=${new Date().getTime()}`,
-);
-document.head.appendChild(truepushSDK);
