6038d2581a98f75e5e9c0ecd4b7ee40d711d25f2
Jorge Soto <Jorgemsm19@gmail.com>
2019-04-14 10:29:23 -0400

    Update media function's media query to be mobile-first

--- frontend/src/lib/utils/styling.js
+++ frontend/src/lib/utils/styling.js
@@ -1,5 +1,5 @@
+/* eslint-disable import/prefer-default-export */
 import { css } from 'styled-components';
-import classnames from 'classnames';
 
 const sizes = {
   desktop: 992,
@@ -10,20 +10,9 @@ const sizes = {
 export const media = Object.keys(sizes).reduce((acc, label) => {
   acc[label] = (...args) => css`
   ${'' /* @media (max-width: ${sizes[label] / 16}em) { */}
-    @media (max-width: ${sizes[label]}px) {
+    @media (min-width: ${sizes[label]}px) {
       ${css(...args)}
     }
   `;
   return acc;
 }, {});
-
-/**
- *  Set ant-menu-item-selected if router's path matches
- *
- * @param {String} path
- * @param {*} router
- */
-export const setActiveLinkClass = (path, router) =>
-  classnames({
-    'ant-menu-item-selected': router.asPath === path,
-  });
