e439f5694bbcf6044db6227e3c7e71f68c03cf96
Jorge Soto <Jorgemsm19@gmail.com>
2019-04-28 13:55:48 -0400

    Add temporary icon to Loading component (change icon later)

--- frontend/src/components/Loading/index.js
+++ frontend/src/components/Loading/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Spinner } from 'styled-icons/fa-solid/Spinner';
 
 import StyledPage from './styles';
 
 const Loading = ({ text }) => (
-  <StyledPage>
-    <p>{text}</p>
+  <StyledPage className='loading'>
+    <div>
+      {/* TODO: Look for a better icon */}
+      <Spinner size={80} className='spinner' />
+      <p>{text}</p>
+    </div>
   </StyledPage>
 );
 
 Loading.defaultProps = {
-  text: 'Loading...',
+  text: '',
 };
 
 Loading.propTypes = {
--- frontend/src/components/Loading/styles.js
+++ frontend/src/components/Loading/styles.js
@@ -8,6 +8,19 @@ const StyledPage = styled.div`
   justify-content: center;
 
   font-size: 2rem;
+
+  div {
+    /* width: 350px; */
+
+    .spinner {
+      display: block;
+      margin: 0 auto;
+      margin-bottom: 10px;
+      color: var(--primary-color);
+
+      animation: 2s linear infinite rotation;
+    }
+  }
 `;
 
 export default StyledPage;
