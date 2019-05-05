9ea885a4512e16ac728d23060c643c9d8f93ebc5
Jorge Soto <Jorgemsm19@gmail.com>
2019-05-04 21:46:17 -0400

    Update styles

--- frontend/src/pages/dashboard/admin/users/styles.js
+++ frontend/src/pages/dashboard/admin/users/styles.js
@@ -3,12 +3,11 @@ import styled from 'styled-components';
 const StyledPage = styled.div`
   height: 100%;
   display: grid;
-  grid-template-rows: auto 1fr;
+  grid-template-rows: auto 93%;
   grid-gap: 1rem;
 
   section {
     display: grid;
-    grid-template-columns: 1fr;
   }
 
   .table-wrapper {
